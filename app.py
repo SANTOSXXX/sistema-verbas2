a93ede60cc9799538542b4a6480f8465182f11b9
SANTOSXXX <93822087+SANTOSXXX@users.noreply.github.com>
2026-01-29 11:46:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,6 @@ import plotly.graph_objects as go
 import time
 from datetime import datetime
 from streamlit_gsheets import GSheetsConnection
-import pygsheets
-
-
-credicias = pygsheets.authorize(service_file="/cred.json")
-
 
 # --- Configuração da Página ---
 st.set_page_config(page_title="Gestão de Verbas (Web)", layout="wide")
@@ -257,5 +252,6 @@ else:
                     time.sleep(1)
                     st.rerun()
                 else: st.error("Índice não existe.")
+
 
 
